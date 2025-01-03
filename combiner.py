b20b1786ddae53937461d5c1954c3741116f4bf5
krstff <krystof.matustik@seznam.cz>
2025-01-03 15:26:43 +0100

    working, but doesnt add silence between samples

--- combiner.py
+++ combiner.py
@@ -19,6 +19,31 @@ def get_files_in_directory(directory):
     
     return files_info
 
+def trim(sound):
+    silences = silence.detect_silence(sound, silence_thresh=0)
+    if len(silences) == 0:
+        return sound
+    elif len(silences) == 1:
+        start = silences[0][0]
+        return sound[start:]
+    else:
+        return sound[silences[0][1]:silences[-1][0]]
+
+def get_max_len(sounds):
+    m_len = 0
+    for sound in sounds:
+        ln = len(sound)
+        if ln > m_len:
+            m_len = ln
+    return m_len
+
+def get_avg_len(sounds):
+    length = 0
+    for s in sounds:
+        length += len(s)
+    print("Average length: ", end="")
+    print(length / len(sounds))
+
 def combiner(dir):
     files = get_files_in_directory(dir)
 
@@ -27,20 +52,27 @@ def combiner(dir):
     
     samples = []
     
+    # Save all samples and get rid of silence
     for file in files:
         sound = AudioSegment.from_file(os.path.join(dir, file["name"] + file["extension"]), format=file["extension"][1:])
-        samples.append(sound)
+        samples.append(trim(sound))
+    
+    get_avg_len(samples)
+    max_len = get_max_len(samples)
+    print("Max length: ", end="")
+    print(max_len)
 
+    # Fill result with silent part if not enough samples were given
+    # so that each sample has the same length
     if len(files) < 16:
         for _ in range(16 - len(files)):
-            samples.append(AudioSegment.silent(2000))
+            samples.append(AudioSegment.silent(max_len))
     
     combined = AudioSegment.empty()
     for sound in samples:
         combined += sound
 
     combined.export("./output.mp3", format="mp3", tags={"artist": "krstff"})
-    # file_handle.close()
 
 if __name__ == "__main__":
 
